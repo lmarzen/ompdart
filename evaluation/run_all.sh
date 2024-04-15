8f09161e31061f766bfef5a4d357bcb7e7bc2fb2
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-04-15 10:00:30 -0500

    added more prints to run_all.sh

--- evaluation/run_all.sh
+++ evaluation/run_all.sh
@@ -6,12 +6,13 @@
 cd src
 
 # bfs
-echo "BENCHMARKING BFS"
+echo "BUILDING BFS"
 cd bfs
 make clean
 make
 rm -rf results
 mkdir results
+echo "BENCHMARKING BFS"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./bfs_offload 4 ../../data/bfs/graph1MW_6.txt ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Compute time:" | awk '{ sum += $3 } END { print "Average Exec Time = " sum/NR" (s)" }'
@@ -24,12 +25,13 @@ cat results/results_naive.out | grep "Compute time:" | awk '{ sum += $3 } END { 
 cd ../
 
 # hotspot
-echo "BENCHMARKING HOTSPOT"
+echo "BUILDING HOTSPOT"
 cd hotspot
 make clean
 make
 rm -rf results
 mkdir results
+echo "BENCHMARKING HOTSPOT"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./hotspot_offload 1024 1024 2 4 ../../data/hotspot/temp_1024 ../../data/hotspot/power_1024 results/output_baseline.out ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Total time:" | awk '{ sum += $3 } END { print "Average Exec Time = " sum/NR" (s)" }'
@@ -42,12 +44,13 @@ cat results/results_naive.out | grep "Total time:" | awk '{ sum += $3 } END { pr
 cd ../
 
 # nw
-echo "BENCHMARKING NW"
+echo "BUILDING NW"
 cd nw
 make clean
 make
 rm -rf results
 mkdir results
+echo "BENCHMARKING NW"
 echo "  RUNNING BASELINE"
 > results/results_baseline.out && for i in {1..30}; do ./needle_offload 2048 10 2 >> results/results_baseline.out ; done
 cat results/results_baseline.out | grep "Total time:" | awk '{ sum += $3 } END { print "Average Exec Time = " sum/NR " (s)" }'
@@ -60,7 +63,7 @@ cat results/results_naive.out | grep "Total time:" | awk '{ sum += $3 } END { pr
 cd ../
 
 # accuracy
-echo "BENCHMARKING ACCURACY"
+echo "BUILDING ACCURACY"
 cd accuracy-omp
 make -f Makefile clean
 make -f Makefile.ompdart clean
@@ -70,6 +73,7 @@ make -f Makefile.ompdart
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING ACCURACY"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do /usr/bin/time -f "real %e" ./main 8192 10000 10 100 ; done > results/results_baseline.out 2>&1
 cat results/results_baseline.out | grep "real" | awk '{ sum += $2 } END { print "Average Exec Time = " sum/NR " (s)" }'
@@ -82,7 +86,7 @@ cat results/results_naive.out | grep "real" | awk '{ sum += $2 } END { print "Av
 cd ../
 
 # ace
-echo "BENCHMARKING ACE"
+echo "BUILDING ACE"
 cd ace-omp
 make -f Makefile clean
 make -f Makefile.ompdart_aggressive clean
@@ -92,6 +96,7 @@ make -f Makefile.ompdart_aggressive
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING ACE"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./main 100 ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Offload time:" | awk '{ sum += $3 } END { print "Average Exec Time = " sum/NR/1000 " (s)" }';q
@@ -104,7 +109,7 @@ cat results/results_naive.out | grep "Offload time:" | awk '{ sum += $3 } END { 
 cd ../
 
 # backprop
-echo "BENCHMARKING BACKPROP"
+echo "BUILDING BACKPROP"
 cd backprop-omp
 make -f Makefile clean
 make -f Makefile.ompdart clean
@@ -114,6 +119,7 @@ make -f Makefile.ompdart
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING BACKPROP"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./main 65536 ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Device offloading time" | awk '{ sum += $5 } END { print "Average Exec Time = " sum/NR " (s)" }'
@@ -126,7 +132,7 @@ cat results/results_naive.out | grep "Device offloading time" | awk '{ sum += $5
 cd ../
 
 # clenergy
-echo "BENCHMARKING CLENERGY"
+echo "BUILDING CLENERGY"
 cd clenergy-omp
 make -f Makefile clean
 make -f Makefile.ompdart clean
@@ -136,6 +142,7 @@ make -f Makefile.ompdart
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING CLENERGY"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./clenergy ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Total time:" | awk '{ sum += $3 } END { print "Average Exec Time = " sum/NR " (s)" }'
@@ -148,7 +155,7 @@ cat results/results_naive.out | grep "Total time:" | awk '{ sum += $3 } END { pr
 cd ../
 
 # xsbench
-echo "BENCHMARKING XSBENCH"
+echo "BUILDING XSBENCH"
 cd xsbench-omp
 make -f Makefile clean
 make -f Makefile.ompdart clean
@@ -158,6 +165,7 @@ make -f Makefile.ompdart
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING XSBENCH"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./XSBench -s large -m event -r 10 ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Runtime:" | awk '{ sum += $2 } END { print "Average Exec Time = " sum/NR " (s)" }'
@@ -170,7 +178,7 @@ cat results/results_naive.out | grep "Runtime:" | awk '{ sum += $2 } END { print
 cd ../
 
 # lulesh
-echo "BENCHMARKING LULESH"
+echo "BUILDING LULESH"
 cd lulesh-omp
 make -f Makefile clean
 make -f Makefile.ompdart clean
@@ -180,6 +188,7 @@ make -f Makefile.ompdart
 make -f Makefile.naive
 rm -rf results
 mkdir results
+echo "BENCHMARKING LULESH"
 echo "  RUNNING BASELINE"
 for i in {1..30}; do ./lulesh -i 100 -s 128 -r 11 -b 1 -c 1 ; done > results/results_baseline.out
 cat results/results_baseline.out | grep "Elapsed time" | awk '{ sum += $4 } END { print "Average Exec Time = " sum/NR " (s)" }'
